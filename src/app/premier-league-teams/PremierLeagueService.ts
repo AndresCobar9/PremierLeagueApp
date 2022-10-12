de48748060ff4d417d3c63d397016e6bbc431c9e
Andres Cobar <110784744+AndresCobar9@users.noreply.github.com>
2022-10-12 13:24:52 -0600

    Update PremierLeagueService.ts

--- src/app/premier-league-teams/PremierLeagueService.ts
+++ src/app/premier-league-teams/PremierLeagueService.ts
@@ -6,7 +6,7 @@ import{ HttpClient} from '@angular/common/http'
 
 export class PremierLeagueTeamsServices
 {
-    private API_ServerTeams="http://127.0.0.1:3000/api/teams";
+    private API_ServerTeams="https://flaskapp-365318.nn.r.appspot.com/api/teams";
     constructor(public http:HttpClient){}
     public getTeams ():Observable<any>{
         return this.http.get(this.API_ServerTeams)
